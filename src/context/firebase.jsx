a0be15ca9a69e66745454d21480fa0d4eebc25f1
AchchansMishra <achchansmishra2003@gmail.com>
2025-11-10 09:46:39 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- src/context/firebase.jsx
+++ src/context/firebase.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaW28sy6-fgim6uQ2KatPL62iVtTcDLXo",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "bookify-f34f0.firebaseapp.com",
   projectId: "bookify-f34f0",
   storageBucket: "bookify-f34f0.firebasestorage.app",
@@ -142,3 +142,4 @@ export const FirebaseProvider = (props) => {
     </FirebaseContext.Provider>
   );
 };
+
